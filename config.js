1f5a429bb876640c13e7218a70454e724bf2e4bb
ankan1092 <65018580+ankan1092@users.noreply.github.com>
2026-09-13 17:48:22 +0530

    Update photo path in config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@
 const CONFIG = {
   // ── Recipient Info ────────────────────────────────────────────
   name: "Eshita",
-  photo: "./img/irene.jpg",       // Place your photo in the img/ folder
+  photo: "./img/eishita.jpg",       // Place your photo in the img/ folder
   music: "./music/hbd.mpeg",      // Place your music in the music/ folder
 
   // ── Theme Colors ──────────────────────────────────────────────
